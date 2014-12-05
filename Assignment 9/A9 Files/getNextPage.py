ee9d88e85297d1972d03799ca7103444b6d8cc12
Holly Harkins <hharkins@cs.odu.edu>
2014-12-05 00:06:36 -0500

    Assignment9

---
+++ Assignment 9/A9 Files/getNextPage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import BeautifulSoup
+import requests
+import os, sys
+
+
+def listOfPageFeedsPageCount(blog):
+
+	feedPages = []
+	if( len(blog) > 0 ):
+
+		if( blog[len(blog)-1] != '/' ):
+			blog = blog + '/feeds/posts/default/'
+		else:
+			blog = blog + 'feeds/posts/default/'
+		
+		listOfPageFeeds = paginate(blog, feedPages)
+
+	
+	return listOfPageFeeds
+
+def paginate(blog, listOfPages=[]):
+
+	html = requests.get(blog)
+	link = BeautifulSoup(html.text).find('link', { 'rel' : 'next' })
+
+	if link is not None:
+		link = link['href']
+		listOfPages.append(link)
+
+		paginate(link, listOfPages)
+	
+	return listOfPages
+out = open('numberOfPages.txt', 'a')
+
+with open("feedlist.txt", "r") as f:
+  for line in f:
+	url = line.strip()
+
+	arrayOfPages = listOfPageFeedsPageCount(url)
+	Number = len(arrayOfPages) + 1
+	print Number
+	out.write(str(Number))
+	out.write('\n') 
+	for page in arrayOfPages:
+		print page
+out.close()
